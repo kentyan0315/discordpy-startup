28f802424882f890ca49a8ad5b9143ba2d2a5abb
kentyan0315 <35226524+kentyan0315@users.noreply.github.com>
2021-01-27 20:06:16 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 import os
 import traceback
-import datetime
+from datetime import datetime, date, timedelta
 import locale
 import calendar
 
@@ -12,33 +12,50 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 channnel_ID = 803873798487146516 #チャンネルID
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
 
+@client.event
+async def on_message(message):
+        if message.content == '/boss':
+        
 
-@bot.command()
-async def test(ctx):
-    await ctx.send('動作中')
+date = datetime.now()
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+month = date.strftime('%m')
+day = date.strftime('%d')
+weekdays = date.strftime('%A')
+
+today = datetime.today()
+
+weekday = date.today().weekday()
+
+weeklist = ['月', '火', '水', '木', '金', '土', '日']
+daylist = [0] * 10
+n = 0
+for n in range(7):
+    daylist[n] = today.date() + timedelta(days=n)
+    
+'''    
+daylist[0] = today.date()
+daylist[1] = today.date() + timedelta(days=1)
+daylist[2] = today.date() + timedelta(days=2)
+daylist[3] = today.date() + timedelta(days=3)
+daylist[4] = today.date() + timedelta(days=4)
+daylist[5] = today.date() + timedelta(days=5)
+daylist[6] = today.date() + timedelta(days=6)
+'''
+num = 0
+while num < 7:
+ #print(date.strftime('%m%d,%A'))
+# day += 1
+
+ if weekday == 7:
+  weekday = 0
+ #print(daylist[num] , weeklist[weekday])
+    await message.channel.send(daylist[num] , weeklist[weekday])
+ weekday += 1 
+ num += 1
     
 
-# 60秒に一回ループ
-@tasks.loop(seconds=60)
-async def loop():
-    # 現在の時刻
-    now = datetime.now().strftime('%A')
-    if now == '2':
-        channel = client.get_channel(channnel_ID)
-        await channel.send('/boss')  
-
-#ループ処理実行
-loop.start()
 
 
 bot.run(token)
