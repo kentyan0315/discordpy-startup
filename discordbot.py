5c2bbf2fac88964e8488ac4dc5cc68c16e2332dc
kentyan0315 <35226524+kentyan0315@users.noreply.github.com>
2021-02-03 17:02:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,7 +6,51 @@ import locale
 import calendar
 import discord
 
+class schedule:
+    def __init__(self,date, today, weekday, weeklist, daylist):
+        self.date = date
+        self.today = today
+        self.weekday = weekday
+        self.weeklist = weeklist
+        self.daylist = daylist
+        
+date = datetime.now()
+# 2018-01-01 00:00:00
+'''
+month = date.strftime('%m')
+day = date.strftime('%d')
+weekdays = date.strftime('%A')
+'''
+today = datetime.today()
 
+weekday = date.today().weekday()
+
+weeklist = ['月', '火', '水', '木', '金', '土', '日']
+daylist = [0] * 10
+n = 0
+for n in range(7):
+    daylist[n] = today.date() + timedelta(days=n)
+    
+'''    
+daylist[0] = today.date()
+daylist[1] = today.date() + timedelta(days=1)
+daylist[2] = today.date() + timedelta(days=2)
+daylist[3] = today.date() + timedelta(days=3)
+daylist[4] = today.date() + timedelta(days=4)
+daylist[5] = today.date() + timedelta(days=5)
+daylist[6] = today.date() + timedelta(days=6)
+'''
+num = 0
+for num in range(7):
+ #print(date.strftime('%m%d,%A'))
+# day += 1
+
+
+ if weekday == 7:
+  weekday = 0
+ print(daylist[num] , weeklist[weekday])
+ weekday += 1 
+ num += 1
 
 
 bot = commands.Bot(command_prefix='/')
@@ -24,4 +68,9 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
     
+
+
+@bot.command()
+async def ping(boss):
+    await ctx.send('daylist[0] = today.date() + timedelta(days=0)')
 bot.run(token)
