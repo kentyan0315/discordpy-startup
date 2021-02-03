22bfeae300b6dd74afd0eacc38c01864b3e779f4
kentyan0315 <35226524+kentyan0315@users.noreply.github.com>
2021-02-03 18:55:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -27,29 +27,14 @@ today = datetime.today()
 
 weekday = date.today().weekday()
 
-day0 = today.date() + timedelta(days=0)
-day1 = today.date() + timedelta(days=1)
-day2 = today.date() + timedelta(days=2)
-day3 = today.date() + timedelta(days=3)
-day4 = today.date() + timedelta(days=4)
-day5 = today.date() + timedelta(days=5)
-day6 = today.date() + timedelta(days=6)
 
-week0 = '月'
-week1 = '火'
-week2 = '水'
-week3 = '木'
-week4 = '金'
-week5 = '土'
-week6 = '日'
 
-'''
 weeklist = ['月', '火', '水', '木', '金', '土', '日']
 daylist = [0] * 10
 n = 0
 for n in range(7):
     daylist[n] = today.date() + timedelta(days=n)
-        
+'''        
 daylist[0] = today.date()
 daylist[1] = today.date() + timedelta(days=1)
 daylist[2] = today.date() + timedelta(days=2)
@@ -86,11 +71,12 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-    
+
+'''
 @bot.command()
 async def boss(ctx, day0)
     await ctx.send(day0)
-
+'''
 
 
 bot.run(token)
